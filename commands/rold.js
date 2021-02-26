cb74c6d0b0173284e7d455b333c1d6f23dae459c
WizziBot <mattrodriguez2000@gmail.com>
2021-02-26 19:00:14 +0000

    eh

--- commands/rold.js
+++ commands/rold.js
@@ -6,7 +6,7 @@ module.exports = {
         try{
             //grabs data
             const raidsData = await raids.findAll();
-            if(raidsData){
+            if(raidsData.length !== 0){
                 for(i = 0; i < raidsData.length; i++){
                     let gName;
                     message.client.guilds.cache.forEach(g => {
@@ -26,6 +26,8 @@ module.exports = {
                     // }
                     message.channel.send(`-----------------------\nName: ${raidsData[i].dataValues.name}\nRaid ID: ${raidsData[i].dataValues.raid_id}\nSublegion: ${gName}\nStart Date: ${raidsData[i].dataValues.start_date}\nEnd Date: ${raidsData[i].dataValues.end_date}\nName: ${raidsData[i].dataValues.description}\n-----------------------`)
                 }
+            } else {
+                message.channel.send(`Raid history is empty.`)
             }
         } catch(e){
             console.trace(e)
